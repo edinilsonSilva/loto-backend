bb7ba01813173335222b170ce3caef99a4cd6192
DESKTOP-D5TPFI4\Meu Computador <edinilsonSilva>
2024-09-24 13:36:26 -0300

    fix: corrigindo cors

--- src/main/java/br/com/loto/config/cors/CorsConfig.java
+++ src/main/java/br/com/loto/config/cors/CorsConfig.java
@@ -1,26 +1,18 @@
-//package br.com.loto.config.cors;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//import java.util.Arrays;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//	@Bean
-//	public CorsConfigurationSource corsConfigurationSource() {
-//		CorsConfiguration configuration = new CorsConfiguration();
-//		configuration.setAllowedOrigins(Arrays.asList("*")); // ou seu domínio
-//		configuration.setAllowedMethods(Arrays.asList("*"));
-//		configuration.setAllowedHeaders(Arrays.asList("*"));
-//
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", configuration);
-//		return source;
-//	}
-//}
+package br.com.loto.config.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas as rotas
+                .allowedOrigins("http://messejana.conectalot.com.br:19880") // Permitir o frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("Content-Type", "Authorization") // Cabeçalhos permitidos
+                .allowCredentials(true); // Permitir cookies ou credenciais
+    }
+}
--- src/main/java/br/com/loto/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/loto/config/security/WebSecurityConfig.java
@@ -12,11 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -104,16 +99,16 @@ public class WebSecurityConfig {
                 .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://messejana.conectalot.com.br:19880/")); // Adicione suas origens permitidas
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Métodos permitidos
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Cabeçalhos permitidos
-        configuration.setAllowCredentials(true); // Permite credenciais (cookies, headers de autorização)
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Aplica a configuração CORS para todas as rotas
-        return source;
-    }
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://messejana.conectalot.com.br:19880/")); // Adicione suas origens permitidas
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Métodos permitidos
+//        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Cabeçalhos permitidos
+//        configuration.setAllowCredentials(true); // Permite credenciais (cookies, headers de autorização)
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration); // Aplica a configuração CORS para todas as rotas
+//        return source;
+//    }
 }
