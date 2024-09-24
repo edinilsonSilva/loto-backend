d6c61312f619f1271511ac2fdb8ee5fc53d80191
DESKTOP-PU3AP7B\Dev <bateraed@gmail.com>
2024-09-24 20:05:07 -0300

    fix: cors

--- src/main/java/br/com/loto/config/cors/CorsConfig.java
+++ src/main/java/br/com/loto/config/cors/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas as rotas
-                .allowedOrigins("*") // Permitir o frontend "http://messejana.conectalot.com.br:19880"
+                .allowedOrigins(
+                        "http://187.19.153.210:19880",
+                        "http://messejana.conectalot.com.br:19880",
+                        "http://localhost:19880"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT") // Métodos permitidos
                 .allowedHeaders("Content-Type", "Authorization") // Cabeçalhos permitidos
                 .allowCredentials(true); // Permitir cookies ou credenciais
